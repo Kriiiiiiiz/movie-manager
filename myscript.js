9b3716d82086f19fbf8bb20be0bd724848c66fbf
lucydct <tenalucia0@gmail.com>
2023-02-07 10:48:19 +0100

    ´welcomepge´

---
+++ myscript.js
@@ -0,0 +1,75 @@
+window.onload = function () {
+    // Variable
+    const IMAGENES = [
+        'assets/slide7.jpg',
+        'assets/slide6.jpg',
+        'assets/slide5.jpg',
+        'assets/slide4.jpg',
+        'assets/slide3.jpg',
+        'assets/slide2.jpg',
+    ];
+    const TIEMPO_INTERVALO_MILESIMAS_SEG = 1000;
+    let posicionActual = 0;
+    let botonRetroceder = document.querySelector('#retroceder');
+    let botonAvanzar = document.querySelector('#avanzar');
+    let imagen = document.querySelector('#imagen');
+    let botonPlay = document.querySelector('#play');
+    let botonStop = document.querySelector('#stop');
+    let intervalo;
+
+    // Funciones
+
+    //Funcion que cambia la foto en la siguiente posicion//
+    function pasarFoto() {
+        if(posicionActual >= IMAGENES.length - 1) {
+            posicionActual = 0;
+        } else {
+            posicionActual++;
+        }
+        renderizarImagen();
+    }
+
+    //Funcion que cambia la foto en la anterior posicion//
+    function retrocederFoto() {
+        if(posicionActual <= 0) {
+            posicionActual = IMAGENES.length - 1;
+        } else {
+            posicionActual--;
+        }
+        renderizarImagen();
+    }
+
+    //Funcion que actualiza la imagen de imagen dependiendo de posicionActual//
+    function renderizarImagen () {
+        imagen.style.backgroundImage = `url(${IMAGENES[posicionActual]})`;
+    }
+
+    //Activa el autoplay de la imagen//
+    function playIntervalo() {
+        intervalo = setInterval(pasarFoto, TIEMPO_INTERVALO_MILESIMAS_SEG);
+        // Desactivamos los botones de control
+        botonAvanzar.setAttribute('disabled', true);
+        botonRetroceder.setAttribute('disabled', true);
+        botonPlay.setAttribute('disabled', true);
+        botonStop.removeAttribute('disabled');
+
+    }
+
+    // Para el autoplay de la imagen//
+    function stopIntervalo() {
+        clearInterval(intervalo);
+        // Activar los botones de control
+        botonAvanzar.removeAttribute('disabled');
+        botonRetroceder.removeAttribute('disabled');
+        botonPlay.removeAttribute('disabled');
+        botonStop.setAttribute('disabled', true);
+    }
+
+    // Events
+  botonAvanzar.addEventListener('click', pasarFoto);
+    botonRetroceder.addEventListener('click', retrocederFoto);
+    botonPlay.addEventListener('click', playIntervalo);
+    botonStop.addEventListener('click', stopIntervalo);
+    // Iniciar
+    renderizarImagen();
+} 
