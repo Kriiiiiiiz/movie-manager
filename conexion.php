c10476bdadedafddef498387db3fd32f986129e8
Kristian <kriiiiiiiz@icloud.com>
2023-02-08 11:19:15 +0100

    Updated db credentials

--- conexion.php
+++ conexion.php
@@ -3,7 +3,7 @@
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'movie_manager');
+define('DB_NAME', 'movie-manager');
 
 $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
