a4a69dc9ea8afbd827f9485be5ecd3ec9b3dbeb5
Berta Garcia <bertagnavarro93@gmail.com>
2023-02-09 17:00:24 +0100

    css login

--- login.php
+++ login.php
@@ -74,6 +74,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="./style.css"/>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 
